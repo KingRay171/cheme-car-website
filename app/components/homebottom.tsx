968d05cafab09a1684d5d2cab07b332db004aee1
Ray Ikome <raycikome@gmail.com>
2024-04-10 12:17:39 -0400

    fixed widths

--- app/components/homebottom.tsx
+++ app/components/homebottom.tsx
@@ -2,14 +2,14 @@ import Image from "next/image"
 
 export default function HomeBottom() {
     return (
-        <section className="flex bg-gray-200 text-black justify-center space-x-[5vw] p-[2vw] w-[100vw]">
+        <section className="flex bg-gray-200 text-black justify-center space-x-[5vw] p-[2vw] max-w-[100vw]">
             <div className="border-solid border-2 border-black rounded-[50px] flex flex-col p-[3vw] w-[50vw]" >
                 <h1 className="text-3xl pb-4 max-w-[40vw]">Upcoming Events:</h1>
                 <ul className="max-w-[40vw] text-2xl "> 
                     <li>we can put our upcoming events/schedule here if we&apos;d like</li>
                 </ul>
             </div>
-            <Image src={"/battery2.png"} width={0} height={0} alt="hi" style={{width: "30vw", height: "auto", objectFit: "contain"}} layout="responsive" unoptimized/>
+            <Image src={"/battery2.png"} width={0} height={0} alt="hi" style={{width: "30vw", height: "auto"}}  unoptimized/>
         </section>
     )
 }
--- app/components/homemiddle.tsx
+++ app/components/homemiddle.tsx
@@ -2,14 +2,14 @@ import Image from "next/image"
 
 export default function HomeMiddle() {
     return (
-        <section className="flex bg-white text-black justify-center space-x-[5vw] p-[2vw]">
+        <section className="flex bg-white text-black justify-center space-x-[5vw] p-[2vw] max-w-[100vw]">
             <div className="border-solid border-2 border-black rounded-[50px] flex flex-col p-[3vw] w-[50vw]">
                 <h1 className="text-3xl">About Us:</h1>
                 <p className="max-w-[40vw] text-2xl"> 
                     description/blurb about the club here
                 </p>
             </div>
-            <Image src={"/battery.jpg"} width={0} height={0} alt="hi" style={{width: "30vw", height: "auto", objectFit: "contain"}} layout="responsive" quality={100}/>
+            <Image src={"/battery.jpg"} width={0} height={0} alt="hi" style={{width: "30vw", height: "auto" }} unoptimized/>
             </section>
     )
 }
